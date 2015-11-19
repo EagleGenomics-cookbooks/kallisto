375b6bbc350c11e66d2a64170be59c0fb798b00f
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-11-19 15:24:42 +0000

    Added tests for building an index and running quantification.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,17 +3,25 @@ kallisto/ CHANGELOG
 
 This file is used to list changes made in each version of the kallisto cookbook.
 
-1.0.0
+1.0.4
 -----
-- [Daniel Barrell] - Initial release of kallisto cookbook
+- [Daniel Barrell] - Added tests for building an index and running quantification.
+
+1.0.3
+-----
+- [Daniel Barrell] - Moved binary install to default and added code to dependencies and source for installing from source. However, kallisto uses a cmd line arg that requires a newer gcc version than the one that ships with cent-os 6.6 so we have stuck to the default binary install for now. Added more tests.
+
+1.0.2
+-----
+- [Daniel Barrell] - Install to /usr/local. Force symbolic link.
 
 1.0.1
 -----
 - [Daniel Barrell] - Updated to kallisto 0.42.4
 
-1.0.2
+1.0.0
 -----
-- [Daniel Barrell] - Install to /usr/local. Force symbolic link.
+- [Daniel Barrell] - Initial release of kallisto cookbook
 
 - - -
 Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -22,5 +22,39 @@ describe command("#{ENV['kallisto_DIR']}/kallisto") do
   its(:stdout) { should contain(ENV['kallisto_VERSION']) }
 end
 
-# TODO: add Building an index, Quantification and Test results:
+# kallisto comes with test data. We can build an index, quantify and test the results:
 # https://pachterlab.github.io/kallisto/starting.html
+describe command("#{ENV['kallisto_DIR']}/kallisto index -i /tmp/transcripts.idx #{ENV['kallisto_DIR']}/test/transcripts.fasta.gz") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe file('/tmp/transcripts.idx') do
+  it { should exist }
+end
+
+describe command("#{ENV['kallisto_DIR']}/kallisto quant -i /tmp/transcripts.idx -o /tmp/output -b 100 #{ENV['kallisto_DIR']}/test/reads_1.fastq.gz #{ENV['kallisto_DIR']}/test/reads_2.fastq.gz") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe file('/tmp/output') do
+  it { should be_directory }
+end
+
+describe file('/tmp/output/abundance.h5') do
+  it { should exist }
+  its(:size) { should eq 292_456 }
+end
+
+describe file('/tmp/output/abundance.tsv') do
+  it { should exist }
+  it { should contain 'NR_003084' }
+  it { should contain '1640' }
+  it { should contain '1462.9' }
+  it { should contain '0.00787013' }
+  it { should contain '0.887453' }
+end
+
+describe file('/tmp/output/run_info.json') do
+  it { should exist }
+  it { should contain(ENV['kallisto_VERSION']) }
+end
